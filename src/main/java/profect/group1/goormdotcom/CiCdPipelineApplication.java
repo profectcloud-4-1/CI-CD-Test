adfb010bef044ff0fff09dec3652c1c198939ad8
ThinkKat <jwy06048@gmail.com>
2025-11-06 13:58:49 +0900

    :wrench: [chore] Initialize spring application

---
+++ src/main/java/profect/group1/goormdotcom/CiCdPipelineApplication.java
@@ -0,0 +1,13 @@
+package profect.group1.goormdotcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdPipelineApplication.class, args);
+	}
+
+}
